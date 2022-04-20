f834e4c8cd6ab547817b6229b43002a84346391c
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-04-20 23:24:42 +0000

    update

---
+++ Template/OuterTemplate/src/components/head.js
@@ -0,0 +1,18 @@
+// import { headStyles } from './misc/head-styles';
+// import { fonts } from './misc/fonts';
+
+
+const head = `<Head/>`;
+
+
+const headComponent = () => {
+
+    // if (!footer) {
+    //     throw new Error('no footer was passed');
+    // }
+
+    return `<Head/>`
+
+}
+
+export { head, headComponent };
