6f7eab8b9afe7a3279a2e5ad4fa30d262c90bf23
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-08-01 12:35:36 +0000

    updates

--- src/display/displayMain.js
+++ src/display/displayMain.js
@@ -6,7 +6,7 @@ import HeadString from './displayHead';
 
 // TODO we need to be able to pass content variable from this
 // file and
-import BodyString from './displayBody';
+import { BodyHTMLString } from './displayBody';
 
 // import EmailTemplateBodyComponent from '../components/bodyComponent';
 
@@ -16,7 +16,7 @@ const content = '[THIS IS PLACE FOR A CONTENT INSIDE]';
 
 let addon1 = {
   head: HeadString,
-  body: BodyString,
+  body: BodyHTMLString,
   // body: EmailTemplateBodyComponent()
   parsedContent: content,
 };
